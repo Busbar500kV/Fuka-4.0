be2e06b2a2139af1cf8e1a05f115769dfe45dfa6
Busbar500kV <yasasponweera@gmail.com>
2025-08-29 17:58:32 -0700

    Update physics.py

--- fuka/physics.py
+++ fuka/physics.py
@@ -28,4 +28,17 @@ def spectral_stats(signal: np.ndarray, fs: float, T_eff: float) -> Dict:
     phi_dom = np.angle(X[k])
 
     return dict(E_sum=E_sum, S_spec=S_spec, F_local=F_local,
-                A_dom=A_dom, w_dom=omega_dom, phi_dom=phi_dom)
+                A_dom=A_dom, w_dom=omega_dom, phi_dom=phi_dom)ඉ
+
+
+def encode_mass(F_before: float, F_after: float, xi_mass: float, c: float):
+    dF = max(0.0, F_before - F_after)
+    dm = xi_mass * dF / (c**2)
+    Q = (1.0 - xi_mass) * dF
+    return dF, dm, Q
+    
+def subtract_mode(signal: np.ndarray, fs: float, A: float, w: float, phi: float) -> np.ndarray:
+    n = len(signal)
+    t = np.arange(n) / fs
+    xhat = A * np.cos(w*t + phi)
+    return signal - xhat
